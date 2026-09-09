a103bd5e219f485da1ec08796d804532a37ba2ed
Fausto <gomezcorderojf@gmail.com>
2026-09-09 10:35:54 -0600

    Initialize ROS master before loading ROS environment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,10 +4,10 @@ set -Eeuo pipefail
 : "${ROBOT_IP:?ROBOT_IP is required}"
 : "${HOST_IP:?HOST_IP is required}"
 
-source /opt/ros/noetic/setup.bash
-source /opt/ros_ws/devel/setup.bash
 export ROS_IP="$HOST_IP"
 export ROS_MASTER_URI="http://${ROBOT_IP}:11311"
+source /opt/ros/noetic/setup.bash
+source /opt/ros_ws/devel/setup.bash
 export PYTHONPATH=/opt/sawyer-control/bridge:${PYTHONPATH:-}
 
 exec python3 -m sawyer_control_bridge.server
